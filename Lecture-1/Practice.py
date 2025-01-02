c2859e3a34c5cd3be214a0328269344a47f08125
Prakriti-Vsk <prakritivish09@gmail.com>
2025-01-02 00:44:11 -0800

    Add files via upload

---
+++ Lecture-1/Practice.py
@@ -0,0 +1,133 @@
+#a1='',"",""" """,''' '''
+#---------------->Basic
+##a1='india'
+##print(a1)
+
+##1a='india'
+##print(1a)
+
+##a a='india'
+##print(a a)
+
+##a_a='india'
+##print(a_a)
+
+##a-a='india'
+##print(a-a)
+
+##a%a='india'
+##print(a%a)
+
+##if='india'
+##print(if)
+
+##else='india'
+##print(else)
+
+#---------------->str,int,float
+
+##a='india'
+##print(a)
+##print(type(a))
+
+##a=45
+##print(a)
+##print(type(a))
+
+##a=45.12
+##print(a)
+##print(type(a))
+
+##a=23
+##b=(a+2)
+##print(b)
+##print(type(b))
+
+##a='23'
+##b=(a+45)
+##print(b)
+##print(type(b))
+
+##a='2a'
+##b=(a+45)
+##print(b)
+##print(type(b))
+
+##a='sachin'
+##print(a*2)
+
+##a='sachin'
+##print(a**2)
+
+#---------------->arithmetic operations
+
+##a=3
+##b=3
+##print(a+b)
+##print(a-b)
+##print(a*b)
+##print(a/b)
+##print(a//b)
+##print(a%b)
+##print(a**b)
+
+#---------------->input
+
+##a=input()
+##print(a)
+
+##a=input('Name :')
+##print(a)
+
+##a=input('Name :')
+##print('Name :',a)
+
+##a=input('Number :')
+##print('Number :',a)
+
+#---------------->conditional statement
+
+##a=int(input('Enter age: '))
+##
+##if(a>18):
+##    print('You  are eligible')
+##elif(a==18):
+##    print('Next Year')
+##else:
+##    print(' not eligible')
+
+##a=int(input('Enter age:'))
+##if(a>18 and a<25):
+##    print('young')
+##else:
+##    print('child/old')
+
+##a=int(input('Enter age:'))
+##if(a>18 or a<23):
+##    print('young')
+##else:
+##    print('child/old')
+
+#---------------->Nested
+
+##a=int(input('Enter num1: '))
+##b=int(input('Enter num1: '))
+##c=int(input('Enter num1: '))
+##if(a>b and a>c):
+##     if(b>c):
+##         print(a,b,c)
+##     else:
+##         print(a,c,b)
+##elif(b>a and b>c):
+##     if(a>c):
+##         print(b,a,c)
+##     else:
+##         print(b,c,a)
+##elif(c>a and c>b):
+##     if(a>b):
+##         print(c,a,b)
+##     else:
+##         print(c,b,a)
+##else:
+##    print('equal values not eligible')
+
