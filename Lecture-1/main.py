1f8d142c28355ce39e465a9c4ffc3aba7731dbdb
Prakriti-Vsk <prakritivish09@gmail.com>
2025-01-01 23:44:31 -0800

    Add files via upload

---
+++ Lecture-1/main.py
@@ -0,0 +1,212 @@
+#a1='',"",""" """,''' '''
+
+##a1='india'
+##print(a1)
+
+##1a='india'
+##print(1a)
+
+##a a='india'
+##print(a a)
+
+##a_a='india'
+##print(a_a)
+
+##-a='india'
+##print(-a)
+
+##+a='india'
+##print(+a)
+
+##if='pragati'
+##print(if)
+#####################str,int,float
+
+##a='india'
+##print(a)
+##print(type(a))
+
+
+##a=10
+##print(a)
+##print(type(a))
+
+
+##a=10.15
+##print(a)
+##print(type(a))
+
+###############casting
+##a='2'
+##b=int(a)
+##print(b+2)
+##print(type(b))
+
+##a='2a'
+##b=int(a)
+##print(b+2)
+##print(type(b))
+
+##a=20
+##b=float(a)
+##print(b+2)
+##print(type(b))
+
+##a=20.23
+##b=int(a)
+##print(b+2)
+##print(type(b))
+##
+
+##a='aman'
+##print(a*2)
+
+#######################
+##a=5
+##b=3
+##print(a+b)
+##print(a-b)
+##print(a/b)
+##print(a//b)
+##print(a*b)
+##print(a**b)
+##print(a%b)
+
+
+################input =str =marksheet,calulcator
+##a=input()
+##print(a)
+
+##a=input('Name: ')
+##print(a)
+
+##a=input('Name: ')
+##print('Name: ',a)
+
+##a=input('number: ')
+##print('number: ',a)
+
+##a=int(input('number: '))
+##print('number: ',a)
+
+######################### conditional statment
+##a=int(input('Enter age: '))
+##
+##if(a>18):
+##    print('you are eligible')
+##else:
+##    print('not eligible')
+
+#################elif
+##a=int(input('Enter age: '))
+##
+##if(a>18):
+##    print('you are eligible')
+##elif(a==18):
+##    print('next time')
+##else:
+##    print('not eligible')
+
+#################and,or
+##a=int(input('Enter age: '))
+##
+##if(a>18 and a<30):
+##    print('young')
+##else:
+##    print('child/old')
+##
+##############or
+##a=int(input('Enter age: '))
+##
+##if(a>18 or a<30):
+##    print('young')
+##else:
+##    print('child/old')
+
+
+#################nested
+##a=int(input('Enter num1: ')) #20
+##b=int(input('Enter num2: ')) #15
+##c=int(input('Enter num3: ')) #10
+##
+##if(a>b and a>c):
+##    if(b>c):
+##        print(a,b,c)
+##    else:
+##        print(a,c,b)
+##elif(b>a and b>c):
+##    if(a>c):
+##        print(b,a,c)
+##    else:
+##        print(b,c,a)
+##elif(c>a and c>b):
+##    if(a>b):
+##        print(c,a,b)
+##    else:
+##        print(c,b,a)
+##else:
+##    print('Nothing')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
