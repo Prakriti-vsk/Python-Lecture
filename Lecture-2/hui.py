9d03da0063a7c0d3181df3b0f469ee6adfde33dc
Prakriti-Vsk <prakritivish09@gmail.com>
2025-01-04 00:27:45 -0800

    Add files via upload

---
+++ Lecture-2/hui.py
@@ -0,0 +1,88 @@
+##a=int(input('Enter number: '))
+##
+##match a:
+##    case 1:
+##        print('+')
+##    case 2:
+##        print('-')
+##    case 3:
+##        print('*')
+##    case 4:
+##        print('/')
+##    case _:
+##        print('nothing')
+
+##a=int(input('Enter number: '))
+##
+##match a:
+##    case _ if(a>18):
+##        print('you are eligible')
+##    case _ if(a==18):
+##        print('next time')
+##    case _:
+##        print('nothing')
+#############################for loop
+#a='india'
+# i n d i a
+# 0 1 2 3 4 -pos
+# -5 -4 -3 -2 -1 -neg
+
+##print(a[0])
+##print(a[0:3])
+##print(a[0:])
+##print(a[:4])
+##print(a[:])
+##print(a[-3])
+##print(a[-4:-1])
+
+
+##for j in a:
+##    print(j)
+
+##for j in a:
+##    print(j,end=' ')
+
+##for j in a:
+##    print(j,end=',')
+
+
+
+##a='india'
+##for k in a:
+##    print(2)
+
+##for i in range(10):
+##    print(i)
+
+
+
+##for i in range(1,10):
+##    print(i)
+
+##for i in range(1,11):
+##    print(i)
+
+ ########################
+
+##for i in range(1,11):
+##    for j in range(i,11):
+##        print(' ',end=' ')
+##    for p in range(1,i+1):
+##        print('*',end=' ')
+##    for h in range(1,i):
+##        print('*',end=' ')
+##    print()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
