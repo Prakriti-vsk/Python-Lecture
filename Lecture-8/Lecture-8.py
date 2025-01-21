bf80466374a6a8b1b3708d276b115f8547c417d7
Prakriti-Vsk <prakritivish09@gmail.com>
2025-01-21 00:24:28 -0800

    Add files via upload

---
+++ Lecture-8/Lecture-8.py
@@ -0,0 +1,84 @@
+#a={'Name':'amit','age':20,'income':45781}
+##print(a)
+##print(type(a))
+##print(len(a))
+
+#print(a[0])
+#print(a['Name'])
+##a['Name']="Neha"
+##print(a)
+##a={'Name':'amit','age':20,'income':45781,'Age':65}
+##print(a)
+
+
+##t=dict(name='aman',age=8)
+##print(t)
+
+##t={
+##   "child1":['amit',20],
+##   'child':['neha',96]
+##}
+##print(t)
+
+##ts={
+##    'child1':{'Name':'amit','age':20},
+##    'child2':{'name':'vinod','age':75}
+##}
+##print(ts)
+
+
+
+##s1={'Name':'amit','age':20}
+##s2={'name':'vinod','age':75}
+##
+##ts={
+##    'child1':s1,
+##    'child2':s2
+##}
+##print(ts)
+##
+#a={'Name':'amit','age':20,'income':45781}
+#a.clear()
+#b=a.copy()
+#print(a.get('age'))
+#print(a.items())
+#print(a.keys())
+#print(a.values())
+#a.pop('Name')
+#del a['Name']
+#a.popitem()
+#a.update({'Name':'Pragati'})
+
+
+##x = ('key1', 'key2', 'key3')
+##y = (10,20,30)
+##
+##thisdict = dict.fromkeys(x, y)
+##
+##print(thisdict)
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.setdefault("school", "Bronco")
+
+print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
