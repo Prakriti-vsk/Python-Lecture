b85c3a7092fb855fd84ca3d4bf098ed498a1a52a
Prakriti-Vsk <prakritivish09@gmail.com>
2025-01-28 00:03:36 -0800

    Add files via upload

---
+++ Lecture-9/Lecture-9.py
@@ -0,0 +1,46 @@
+import datetime
+
+x = datetime.datetime.now()
+#y=x.strftime("%a")
+#y=x.strftime("%A")
+#y=x.strftime("%w")
+#y=x.strftime("%d")
+#y=x.strftime("%b")
+#y=x.strftime("%B")
+#y=x.strftime("%M")
+#y=x.strftime("%m")
+#y=x.strftime("%y")
+#y=x.strftime("%Y")
+#y=x.strftime("%H")
+#y=x.strftime("%I")
+#y=x.strftime("%p")
+#y=x.strftime("%S")
+#y=x.strftime("%f")
+#y=x.strftime("%j")
+#y=x.strftime("%U")
+#y=x.strftime("%c")
+#y=x.strftime("%C")
+#y=x.strftime("%x")
+#y=x.strftime("%X")
+#y=x.strftime("%x")
+#y=x.strftime("%G")
+#y=x.strftime("%u")
+#y=x.strftime("%V")
+#print(y)
+
+import time
+time.sleep(60)
+print('Hello world')
+
+
+
+
+
+
+
+
+
+
+
+
+
