44fcb467efe815cd6fcc23060194fb4b35aafc42
Prakriti-Vsk <prakritivish09@gmail.com>
2025-04-15 01:47:49 -0700

    Add files via upload

---
+++ Assignment/to-dolist solution.py
@@ -0,0 +1,62 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:/Users/Admin/pragati/to-do list.py ================
+
+===== TO-DO LIST MENU =====
+1. View tasks
+2. Add task
+3. Mark task as completed
+4. Delete task
+5. Exit
+Choose an option (1-5): 1
+
+----- YOUR TASKS -----
+1. internship [Done]
+
+===== TO-DO LIST MENU =====
+1. View tasks
+2. Add task
+3. Mark task as completed
+4. Delete task
+5. Exit
+Choose an option (1-5): 2
+Enter task: Classes work
+Task added.
+
+===== TO-DO LIST MENU =====
+1. View tasks
+2. Add task
+3. Mark task as completed
+4. Delete task
+5. Exit
+Choose an option (1-5): 3
+
+----- YOUR TASKS -----
+1. internship [Done]
+2. Classes work [Not Done]
+Enter task number to mark as completed: 2
+Task marked as completed.
+
+===== TO-DO LIST MENU =====
+1. View tasks
+2. Add task
+3. Mark task as completed
+4. Delete task
+5. Exit
+Choose an option (1-5): 4
+
+----- YOUR TASKS -----
+1. internship [Done]
+2. Classes work [Done]
+Enter task number to delete: 1
+Deleted task: internship
+
+===== TO-DO LIST MENU =====
+1. View tasks
+2. Add task
+3. Mark task as completed
+4. Delete task
+5. Exit
+Choose an option (1-5): 5
+Tasks saved. Exiting To-Do List.
